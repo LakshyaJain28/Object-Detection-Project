57af0403a77e9565ce63a224107fb7075da37fd0
LakshyaJain28 <79844523+LakshyaJain28@users.noreply.github.com>
2022-01-23 17:02:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function one()
+{
+    window.location="tv.html";
+}
+function now()
+{
+    window.location="fruitb.html";
+}
+function two()
+{
+    window.location="bedroom.html";
+}
+function five()
+{
+    window.location="desk.html";
+}
+function four()
+{
+    window.location="b.html";
+}
